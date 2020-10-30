0106956a2c43def34d329fb7c9360be3b63b37cc
Bilinski-Jakub <73522953+BilinskiJakub@users.noreply.github.com>
2020-10-30 15:40:35 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,11 +1,7 @@
 <?php
 
-$servername = "mysql-bilinski-jakub.alwaysdata.net";
-$username = "217212";
-$password = "haslo_testowe";
-$dbanme = "bilinski-jakub_test";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli('mysql-bilinski-jakub.alwaysdata.net','217212','haslo_testowe','bilinski-jakub_test');
 
 if ($conn->connect_error) {
   die("Connection failed: ".mysql_connect_error());
