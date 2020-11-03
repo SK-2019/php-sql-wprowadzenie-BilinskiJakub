89e3b1f06f9aefb24b9664cc985c0f54d31f52eb
Tomjand <domagala.tomasz@gmail.com>
2020-11-03 10:02:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,20 +16,20 @@
    echo("<h1> Zadanie nr 1  |  SELECT * FROM pracownicy </h1>");
 
     require_once('connect.php');
-//     $result = $conn->query('SELECT * FROM pracownicy');
-//         echo("<table border=1>");
-//         echo("<th>Id</th>"); 
-//         echo("<th>Imie</th>");
-//         echo("<th>Dzial</th>");
-//         echo("<th>Zarobki</th>");
-//             while($row=$result->fetch_assoc()){ 
-//                 echo("<tr>");
-//                     echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
+    $result = $conn->query('SELECT * FROM pracownicy');
+        echo("<table border=1>");
+        echo("<th>Id</th>"); 
+        echo("<th>Imie</th>");
+        echo("<th>Dzial</th>");
+        echo("<th>Zarobki</th>");
+            while($row=$result->fetch_assoc()){ 
+                echo("<tr>");
+                    echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>"); 
 
-//                 echo("</tr>");
-//             }
+                echo("</tr>");
+            }
 
-//         echo("</table>");
+        echo("</table>");
 
 //         echo("<h1> Zadanie nr 2  |  SELECT * FROM pracownicy where imie like '%a' </h1>");
 
