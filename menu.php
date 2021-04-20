61aef426d0898528f527b619bda88a9efc410b32
Tomjand <domagala.tomasz@gmail.com>
2021-04-20 09:39:28 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Assets/style.css">
-    <title>Insert</title>
-</head>
-<body>
-
-<div class="nav">
-         <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-BilinskiJakub" >GITHUB</a>
+        <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-BilinskiJakub" >GITHUB</a>
             <a class="menu" href="index.php">Index</a>
             <a class="menu" href="/Pracownicy/pracownicy.php">Pracownicy</a>
             <a class="menu" href="/Pracownicy/organizacja.php">Pracownicy i Organizacja</a>
@@ -19,6 +8,4 @@
             <a class="menu" href="/Formularze/DaneDoBazy.php">DaneDoBazy</a>
             <a class="menu" href="/Biblioteka/ksiazki.php">Książki</a>
             <a class="menu" href="/Flexbox/index.html">Flexbox</a>
-        </div>
-        </body>
-        </html>
+
