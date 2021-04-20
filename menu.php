e537385bce6ffc27c0768c09f279438d15d9def8
Bilinski-Jakub <73522953+BilinskiJakub@users.noreply.github.com>
2021-04-20 09:33:12 +0200

    Menu.php

---
+++ menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Assets/style.css">
+    <title>Insert</title>
+</head>
+<body>
+
+<div class="nav">
+         <a class="menu" href="https://github.com/SK-2019/php-sql-wprowadzenie-BilinskiJakub" >GITHUB</a>
+            <a class="menu" href="index.php">Index</a>
+            <a class="menu" href="/Pracownicy/pracownicy.php">Pracownicy</a>
+            <a class="menu" href="/Pracownicy/organizacja.php">Pracownicy i Organizacja</a>
+            <a class="menu" href="/Pracownicy/funkcje.php">Funkcje agregujące</a>
+            <a class="menu" href="/Pracownicy/dataczas.php">Data i Czas</a>
+            <a class="menu" href="/Formularze/formularz.html">Formularz</a>
+            <a class="menu" href="/Formularze/DaneDoBazy.php">DaneDoBazy</a>
+            <a class="menu" href="/Biblioteka/ksiazki.php">Książki</a>
+            <a class="menu" href="/Flexbox/index.html">Flexbox</a>
+        </div>
+        </body>
+        </html>
